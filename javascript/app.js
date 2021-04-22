67a445ff6a467ad72a7674b173fb81631516cf0f
Ильяс <ilyas-nugaev@mail.ru>
2021-04-22 17:32:10 +0300

    added switch

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,13 @@
 var main = function () {
 	"use strict";
-	console.log("hello, world!");
+	var tabNumber;
+	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+		$(tabSelector).on("click", function () {
+			$(".tabs span").removeClass("active");
+			$(this).addClass("active");
+			return false;
+		});
+	}
 };
-$(document).ready(main);
+main();
