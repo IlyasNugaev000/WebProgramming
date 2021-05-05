b34041496a8207f2fd3fdf49e977177fba17fffa
Ильяс <ilyas-nugaev@mail.ru>
2021-05-05 14:45:45 +0300

    Creating an intermediate tag data structure

--- Amazeriffic/client/javascript/app.js
+++ Amazeriffic/client/javascript/app.js
@@ -1,3 +1,30 @@
+var tagIndex = function (tag, tagsList) {
+	var i;
+	for (i=0;i<tagsList.length;i++) {
+		if (tagsList[i].name.localeCompare(tag)==0) return i;
+	}
+	return -1;
+};
+
+
+var organizeByTags = function (toDosList) {
+	var organizedByTags = [];
+	toDosList.forEach(function (toDoItem) { // toDoItem = { "description":"...", "tags":[...] }
+		toDoItem.tags.forEach(function (tagItem) { // tagItem="..."
+			var ind=tagIndex(tagItem, organizedByTags);
+			if (ind==-1) {
+				organizedByTags.push({
+					"name": tagItem,
+					"toDos": [toDoItem.description]
+				});
+			}
+			else organizedByTags[ind].toDos.push(toDoItem.description);
+		});
+	});
+	return organizedByTags;
+};
+
+
 var main = function (toDoObjects) {
 	"use strict";
 	var toDos = toDoObjects.map(function (toDo) {
@@ -31,38 +58,7 @@ var main = function (toDoObjects) {
 
 			else if ($element.parent().is(":nth-child(3)")) {
 				console.log("Щелчок на вкладке Теги");
-				var organizedByTag = [
-				{
-					"name" : "покупки",
-					"toDos" : ["Купить продукты"]
-				},
-				{
-					"name" : "рутина",
-					"toDos" : ["Купить продукты", 
-							   "Вывести Грейси на прогулку в парк"]
-				},
-				{
-					"name" : "писательство",
-					"toDos" : ["Сделать несколько новых задач", 
-								"Закончить писать книгу"]
-				},
-				{
-					"name" : "работа",
-					"toDos" : ["Сделать несколько новых задач", 
-								"Подготовиться к лекциив понедельник",
-								"Ответить на электронные письма", 
-								"Закончить писать книгу"]
-				},
-				{
-					"name" : "преподавание",
-					"toDos" : ["Подготовиться к лекции в понедельник"]
-				},
-				{
-					"name" : "питомцы",
-					"toDos" : ["Вывести Грейси на прогулку в парк"]
-				}
-
-				];
+				var organizedByTag = organizeByTags(toDoObjects);
 
 				organizedByTag.forEach(function (tag) {
 					var $tagName=$("<h3>").text(tag.name);
