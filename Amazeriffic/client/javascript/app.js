405cf2ee126c747e71d3c8a880af3f600b06b81f
Ильяс <ilyas-nugaev@mail.ru>
2021-05-05 10:22:10 +0300

    add tags

--- Amazeriffic/client/javascript/app.js
+++ Amazeriffic/client/javascript/app.js
@@ -10,23 +10,35 @@ var main = function (toDoObjects) {
 			$(".tabs a span").removeClass("active");
 			$element.addClass("active");
 			$("main .content").empty();
+
 			if ($element.parent().is(":nth-child(1)")) {
 				$content = $("<ul>");
+
 				for (var i = toDos.length-1; i > -1; i--) {
 					$content.append($("<li>").text(toDos[i]));
 				}
+
 				$("main .content").append($content);
-			} else if ($element.parent().is(":nth-child(2)")) {
+			}
+
+			else if ($element.parent().is(":nth-child(2)")) {
 				$content = $("<ul>");
 				toDos.forEach(function (todo) {
 					$content.append($("<li>").text(todo));
 				});
 				$("main .content").append($content);
-			} else if ($element.parent().is(":nth-child(3)")) {
+			} 
+
+			else if ($element.parent().is(":nth-child(3)")) {
+				console.log("Щелчок на вкладке Теги");
+			} 
+
+			else if ($element.parent().is(":nth-child(4)")) {
 				$("main .content").append('<div class="container"> <div class="container__item"> <input class="form__field" placeholder="Новая задача" /> <button type="button" class="input_btn">+</button> </div> </div>');
 				$(".input_btn").on("click", function () {
 					if (($(".form__field").val() !== "") && (($(".form__field").val()).trim().length > 0)){
 						var newToDo = $('.form__field').val();
+
 						if (newToDo != '') {
 							toDos.push( newToDo);
 							alert('Новое задание "'+newToDo+'" успешно добавлено!');
@@ -39,6 +51,7 @@ var main = function (toDoObjects) {
 					if (event.keyCode === 13) {
 						if (($(".form__field").val() !== "") && (($(".form__field").val()).trim().length > 0)){
 							var newToDo= $('.form__field').val();
+							
 							if (newToDo!='') {
 								toDos.push( newToDo);
 								alert('Новое задание "'+newToDo+'" успешно добавлено!');
